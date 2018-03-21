034197076b8bfafe61079bbb6386b95962c14f4f
Ben Del Vacchio <ben@yesenergy.com>
2018-03-21 16:44:45 -0600

    Updating app

--- src/app.js
+++ src/app.js
@@ -7,13 +7,7 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 import { addExpense, removeExpense, editExpense } from './actions/expenses';
-import {
-	setTextFilter,
-	sortByDate,
-	sortByAmount,
-	setStartDate,
-	setEndDate
-} from './actions/filters';
+import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 
 const store = configureStore();
@@ -31,27 +25,3 @@ store.subscribe(() => {
 	const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 	// console.log(visibleExpenses);
 });
-
-const expense1 = store.dispatch(
-	addExpense({
-		description: 'Water Bill',
-		amount: 5000,
-		createdAt: 1000
-	})
-);
-
-const expense2 = store.dispatch(
-	addExpense({
-		description: 'Gas Bill',
-		amount: 2000,
-		createdAt: 1100
-	})
-);
-
-const expense3 = store.dispatch(
-	addExpense({
-		description: 'Rent',
-		amount: 109500,
-		createdAt: 1500
-	})
-);
