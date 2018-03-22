31cdada52042d260f96d8addc63c3098ab7d48f4
Ben Del Vacchio <ben@yesenergy.com>
2018-03-22 10:12:38 -0600

    Webpack

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,6 +1,39 @@
 import selectExpenses from '../../selectors/expenses';
 
+const expenses = [
+	{
+		id: 1,
+		description: 'Gum',
+		note: '',
+		amount: 195,
+		createdAt: 0
+	},
+	{
+		id: 2,
+		description: 'Rent',
+		note: '',
+		amount: 1095,
+		createdAt: -1000
+	},
+	{
+		id: 3,
+		description: 'Credit card',
+		note: '',
+		amount: 4500,
+		createdAt: 1000
+	}
+];
+
 test('should filter by text value', () => {
+	const filters = {
+		text: 'e',
+		sortBy: 'date',
+		startDate: undefined,
+		endDate: undefined
+	};
+
+	const result = selectExpenses(expenses, filters);
+
 	// expect(setEndDate(moment(0))).toEqual({
 	// 	type: 'SET_END_DATE',
 	// 	endDate: moment(0)
